b194206242955c59271d0b50de5a24623a2c0c16
= <alexandressoares383@gmail.com>
2024-06-07 12:22:59 -0300

    chore: first commit

---
+++ igniteteams/src/screens/Groups/index.tsx
@@ -0,0 +1,11 @@
+import { Text, View } from 'react-native';
+
+import styles from './styles'
+
+export default function Groups() {
+  return (
+    <View style={styles.container}>
+      <Text>Groups</Text>
+    </View>
+  );
+}
