68cce42d86fdcdc1a6df6c6ff364b5f4f4f3f63b
= <alexandressoares383@gmail.com>
2024-06-07 16:50:15 -0300

    feat: componente loading

--- igniteteams/src/screens/Groups/index.tsx
+++ igniteteams/src/screens/Groups/index.tsx
@@ -1,9 +1,7 @@
-import { Container, Title } from './styles'
+import { Container } from './styles'
 
 export default function Groups() {
   return (
-    <Container>
-      <Title>Groups React Native</Title>
-    </Container>
+    <Container></Container>
   );
 }
--- igniteteams/src/screens/Groups/styles.ts
+++ igniteteams/src/screens/Groups/styles.ts
@@ -1,13 +1,7 @@
 import styled from "styled-components/native";
 
 export const Container = styled.View`
-  background-color: #000000;
+  background-color: ${({ theme }) => theme.COLORS.GRAY_600};
   flex: 1;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Title = styled.Text`
-  color: #fff;
-  font-size: 32px;
+  padding: 24px;
 `;
