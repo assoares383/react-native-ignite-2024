af1b0adbab2682ac7743e1188be50bb11bd63ed7
= <alexandressoares383@gmail.com>
2024-06-10 17:35:14 -0300

    feat: criando componente input

--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -3,6 +3,7 @@ import { Container, Content, Icon } from './styles'
 import { Button } from '@components/Button';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/HighLight';
+import { Input } from '@components/Input';
 
 export default function NewGroup() {
   return (
@@ -16,8 +17,11 @@ export default function NewGroup() {
                 subtitle='Crie a turma para adicionar as pessoas'
             />
 
+            <Input />
+
             <Button 
                 title="Criar"
+                style={{ marginTop: 20 }}
             />
         </Content>
     </Container>
