1d3f98af67eee37bf50e1f61a20e16dad0d3533c
= <alexandressoares383@gmail.com>
2024-06-12 20:33:15 -0300

    feat: listando os filtros

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -1,4 +1,5 @@
-import { Container, Form } from './styles'
+import { useState } from 'react';
+import { FlatList } from 'react-native'
 
 import { ButtonIcon } from '@components/ButtonIcon';
 import { Filter } from '@components/Filter';
@@ -6,7 +7,12 @@ import { Header } from '@components/Header';
 import { Highlight } from '@components/HighLight';
 import { Input } from '@components/Input';
 
+import { Container, Form, HeaderList, NumbersOfPlayers } from './styles'
+
 export default function Players() {
+  const [team, setTeam] = useState('Time A');
+  const [players, setPlayers] = useState([]);
+
   return (
     <Container>
       <Header showBackButton />
@@ -25,10 +31,23 @@ export default function Players() {
         <ButtonIcon icon='add' />
       </Form>
 
-      <Filter 
-        title="Time  A"
-        isActive
-      />
+      <HeaderList>
+        <FlatList 
+          data={['Time A', 'Time B']}
+          keyExtractor={item => item}
+          renderItem={({ item }) => (
+            <Filter 
+              title={item}
+              isActive={item === team}
+              onPress={() => setTeam(item)}
+            />
+          )}
+          horizontal
+        />
+        <NumbersOfPlayers>
+          {players.length}
+        </NumbersOfPlayers>
+      </HeaderList>
     </Container>
   );
 }
