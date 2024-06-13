d4ec4798eee163c1f2ffa054625e1a95360e17e1
= <alexandressoares383@gmail.com>
2024-06-12 21:04:14 -0300

    feat: add componente playercard

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -6,12 +6,13 @@ import { Filter } from '@components/Filter';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/HighLight';
 import { Input } from '@components/Input';
+import { PlayerCard } from '@components/PlayerCard';
 
 import { Container, Form, HeaderList, NumbersOfPlayers } from './styles'
 
 export default function Players() {
   const [team, setTeam] = useState('Time A');
-  const [players, setPlayers] = useState([]);
+  const [players, setPlayers] = useState(['Alexandre', 'Leticia']);
 
   return (
     <Container>
@@ -48,6 +49,14 @@ export default function Players() {
           {players.length}
         </NumbersOfPlayers>
       </HeaderList>
+
+      <FlatList 
+        data={players}
+        keyExtractor={item => item}
+        renderItem={({ item }) => (
+          <PlayerCard name={item} />
+        )}
+      />
     </Container>
   );
 }
