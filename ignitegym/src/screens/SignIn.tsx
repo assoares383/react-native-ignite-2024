dae1eca43642d2ef60afbce2640ee32e65f573ef
= <alexandressoares383@gmail.com>
2024-06-25 22:33:46 -0300

    feat: utilizando svg

--- ignitegym/src/screens/SignIn.tsx
+++ ignitegym/src/screens/SignIn.tsx
@@ -1,4 +1,6 @@
-import { Image, VStack  } from 'native-base';
+import { Center, Image, Text, VStack  } from 'native-base';
+
+import LogoSvg from '@assets/logo.svg';
 
 import BackgroundImg from '@assets/background.png';
 
@@ -11,6 +13,13 @@ export function SignIn() {
                 resizeMode='cover' 
                 position="absolute" 
             />
+            <Center my={24}>
+                <LogoSvg />
+
+                <Text color="gray.100" fontSize="sm">
+                    Treien sua mente e o seu corpo
+                </Text>
+            </Center>
         </VStack>
     )
 }
