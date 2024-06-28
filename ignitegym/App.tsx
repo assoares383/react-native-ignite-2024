7e20c3136686da17cab4ed70ccd5eb8596783ec2
= <alexandressoares383@gmail.com>
2024-06-28 19:35:43 -0300

    feat: criando a estrutura das interfaces do app

--- ignitegym/App.tsx
+++ ignitegym/App.tsx
@@ -6,6 +6,7 @@ import { Routes } from './src/routes';
 
 import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
+import { Home } from './src/screens/Home';
 
 export default function App() {
   const [ fontsLoaded ] = useFonts({ Roboto_400Regular, Roboto_700Bold });
@@ -17,7 +18,7 @@ export default function App() {
         backgroundColor="transparent"
         translucent
       />
-      { fontsLoaded ? <Routes /> : <Loading /> }
+      { fontsLoaded ? <Home /> : <Loading /> }
     </NativeBaseProvider>
   );
 }
