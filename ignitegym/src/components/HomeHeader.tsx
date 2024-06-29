b6bfc6be695cfbe76eb4a19f8ed42dbabb8d7672
= <alexandressoares383@gmail.com>
2024-06-29 15:29:17 -0300

    feat: estilizando o HomeHeader

--- ignitegym/src/components/HomeHeader.tsx
+++ ignitegym/src/components/HomeHeader.tsx
@@ -2,13 +2,13 @@ import { Heading, HStack, VStack, Text } from "native-base";
 
 export function HomeHeader() {
     return (
-        <HStack>
+        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
             <VStack>
-                <Text color="gray.100">
+                <Text color="gray.100" fontSize="md">
                     Ola, 
                 </Text>
 
-                <Heading color="gray.100">
+                <Heading color="gray.100" fontSize="md">
                     Alexandre
                 </Heading>
             </VStack>
