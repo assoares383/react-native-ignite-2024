2c1df5afff47d2fefb965692387bed5f03527e58
= <alexandressoares383@gmail.com>
2024-06-29 15:38:05 -0300

    feat: add componente userPhoto

--- ignitegym/src/components/HomeHeader.tsx
+++ ignitegym/src/components/HomeHeader.tsx
@@ -1,8 +1,16 @@
 import { Heading, HStack, VStack, Text } from "native-base";
 
+import { UserPhoto } from "./UserPhoto";
+
 export function HomeHeader() {
     return (
         <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+            <UserPhoto 
+                source={{ uri: 'https://avatars.githubusercontent.com/u/15836394?v=4' }}
+                alt="Imagem do Usuario"
+                size={16}
+                mr={4}
+            />
             <VStack>
                 <Text color="gray.100" fontSize="md">
                     Ola, 
