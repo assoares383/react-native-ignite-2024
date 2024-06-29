0807a24907101941e4e0975a3e3af68f0d93806c
= <alexandressoares383@gmail.com>
2024-06-28 22:29:53 -0300

    feat: removendo a label do menu

--- ignitegym/src/routes/app.routes.tsx
+++ ignitegym/src/routes/app.routes.tsx
@@ -1,40 +1,31 @@
-import { createBottomTabNavigator, BottomTabNavigationProp } from '@react-navigation/bottom-tabs';
+import {
+  createBottomTabNavigator,
+  BottomTabNavigationProp,
+} from "@react-navigation/bottom-tabs";
 
-import { Exercise } from '../screens/Exercise';
-import { History } from '../screens/History';
-import { Home } from '../screens/Home';
-import { Profile } from '../screens/Profile';
+import { Exercise } from "../screens/Exercise";
+import { History } from "../screens/History";
+import { Home } from "../screens/Home";
+import { Profile } from "../screens/Profile";
 
 type AppRoutes = {
-    home: undefined;
-    exercise: undefined;
-    profile: undefined;
-    history: undefined;
-}
+  home: undefined;
+  exercise: undefined;
+  profile: undefined;
+  history: undefined;
+};
 
 export type AppNavigatorRoutesProp = BottomTabNavigationProp<AppRoutes>;
 
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
-    return (
-        <Navigator screenOptions={{ headerShown: false }}>
-            <Screen 
-                name="home"
-                component={Home}
-            />
-            <Screen 
-                name="history"
-                component={History}
-            />
-            <Screen 
-                name="profile"
-                component={Profile}
-            />
-            <Screen 
-                name="exercise"
-                component={Exercise}
-            />
-        </Navigator>
-    )
+  return (
+    <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
+      <Screen name="home" component={Home} />
+      <Screen name="history" component={History} />
+      <Screen name="profile" component={Profile} />
+      <Screen name="exercise" component={Exercise} />
+    </Navigator>
+  );
 }
