e909ff6f5b6243d492f4486c42ce1a8cf4c8130a
= <alexandressoares383@gmail.com>
2024-06-28 22:50:56 -0300

    feat: alterando os icones da bottom navigator

--- ignitegym/src/routes/app.routes.tsx
+++ ignitegym/src/routes/app.routes.tsx
@@ -1,7 +1,12 @@
+import { useTheme } from "native-base";
 import {
   createBottomTabNavigator,
   BottomTabNavigationProp,
 } from "@react-navigation/bottom-tabs";
+
+import HomeSvg from "@assets/home.svg";
+import ProfileSvg from "@assets/profile.svg";
+import HistorySvg from "@assets/history.svg";
 
 import { Exercise } from "../screens/Exercise";
 import { History } from "../screens/History";
@@ -20,12 +25,42 @@ export type AppNavigatorRoutesProp = BottomTabNavigationProp<AppRoutes>;
 const { Navigator, Screen } = createBottomTabNavigator<AppRoutes>();
 
 export function AppRoutes() {
+  const { sizes } = useTheme();
+  const iconSize = sizes[6];
+
   return (
     <Navigator screenOptions={{ headerShown: false, tabBarShowLabel: false }}>
-      <Screen name="home" component={Home} />
-      <Screen name="history" component={History} />
-      <Screen name="profile" component={Profile} />
-      <Screen name="exercise" component={Exercise} />
+      <Screen
+        name="home"
+        component={Home}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen
+        name="history"
+        component={History}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HistorySvg fill={color} width={iconSize} height={iconSize} />
+          ),
+        }}
+      />
+      <Screen 
+        name="profile"
+        component={Profile}
+        options={{
+            tabBarIcon: ({ color }) => (
+              <ProfileSvg fill={color} width={iconSize} height={iconSize} />
+            ),
+          }}
+      />
+      <Screen
+        name="exercise"
+        component={Exercise}
+      />
     </Navigator>
   );
 }
