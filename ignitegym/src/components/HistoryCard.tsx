3b5013575934c39eec76e2a194cc62120b9bddfd
= <alexandressoares383@gmail.com>
2024-06-29 17:51:54 -0300

    feat: add componente HistoryCard

---
+++ ignitegym/src/components/HistoryCard.tsx
@@ -0,0 +1,28 @@
+import { HStack, Heading, Text, VStack } from "native-base";
+
+export function HistoryCard() {
+    return (
+        <HStack 
+            w="full" 
+            px={4} 
+            mb={3} 
+            bg="gray.600" 
+            rounded="md" 
+            alignItems="center"
+            justifyContent="space-between"
+        >
+            <VStack mr={5}>
+                <Heading color="white" fontSize="md" textTransform="capitalize">
+                    Costas
+                </Heading>
+                <Text color="gray.200" fontSize="lg" numberOfLines={1}>
+                    Puxada frontal
+                </Text>
+            </VStack>
+
+            <Text color="gray.300" fontSize="md">
+                08:56
+            </Text>
+        </HStack>
+    );
+}
