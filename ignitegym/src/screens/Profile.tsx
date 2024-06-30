cfc7179f7143d6a85b41460f060a38e2940f1224
= <alexandressoares383@gmail.com>
2024-06-30 16:12:12 -0300

    feat: foto do usuario

--- ignitegym/src/screens/Profile.tsx
+++ ignitegym/src/screens/Profile.tsx
@@ -1,9 +1,23 @@
-import { Center, Text} from 'native-base';
+import { Center, ScrollView, VStack } from "native-base";
+
+import { ScreenHeader } from "../components/ScreenHeader";
+import { UserPhoto } from "../components/UserPhoto";
 
 export function Profile() {
-    return (
-        <Center flex={1}>
-            <Text color="white">Profile</Text>
+  return (
+    <VStack flex={1}>
+      <ScreenHeader title="Perfil" />
+      <ScrollView>
+        <Center mt={6} px={10}>
+            <UserPhoto
+                source={{
+                    uri: "https://avatars.githubusercontent.com/u/15836394?v=4",
+                }}
+                alt="Imagem do Usuario"
+                size={33}
+            />
         </Center>
-    )
+      </ScrollView>
+    </VStack>
+  );
 }
