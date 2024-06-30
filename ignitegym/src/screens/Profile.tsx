c0d87fb7de4208fcf69c4864c7068bc7b01095cd
= <alexandressoares383@gmail.com>
2024-06-30 18:26:02 -0300

    feat: inputs de senha

--- ignitegym/src/screens/Profile.tsx
+++ ignitegym/src/screens/Profile.tsx
@@ -1,11 +1,19 @@
 import { useState } from "react";
 import { TouchableOpacity } from "react-native";
 
-import { Center, ScrollView, Skeleton, Text, VStack } from "native-base";
+import {
+  Center,
+  Heading,
+  ScrollView,
+  Skeleton,
+  Text,
+  VStack,
+} from "native-base";
 
 import { ScreenHeader } from "../components/ScreenHeader";
 import { UserPhoto } from "../components/UserPhoto";
-import { Input } from '../components/Input';
+import { Button } from "../components/Button";
+import { Input } from "../components/Input";
 
 const PHOTO_SIZE = 33;
 
@@ -36,21 +44,37 @@ export function Profile() {
           )}
 
           <TouchableOpacity>
-            <Text color="green.500" fontWeight="bold" fontSize="md" mt={2} mb={8}>
-                Alterar foto
+            <Text
+              color="green.500"
+              fontWeight="bold"
+              fontSize="md"
+              mt={2}
+              mb={8}
+            >
+              Alterar foto
             </Text>
           </TouchableOpacity>
 
-          <Input 
-            bg="gray.600"
-            placeholder="Nome"
-          />
-          <Input 
-            bg="gray.600"
-            placeholder="E-mail"
-            isDisabled
-          />
+          <Input bg="gray.600" placeholder="Nome" />
+          <Input bg="gray.600" placeholder="E-mail" isDisabled />
         </Center>
+
+        <VStack px={10} mt={12} mb={9}>
+          <Heading color="gray.200" fontSize="md" mb={2}>
+            Alterar senha
+          </Heading>
+
+          <Input bg="gray.600" placeholder="Senha antiga" secureTextEntry />
+
+          <Input bg="gray.600" placeholder="Nova senha" secureTextEntry />
+
+          <Input bg="gray.600" placeholder="Confirme a nova senha" secureTextEntry />
+
+          <Button 
+            title="Atualizar"
+            mt={4}
+          />
+        </VStack>
       </ScrollView>
     </VStack>
   );
