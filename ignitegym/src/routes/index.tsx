c63f98a8d609510c6c13656a930223a347c040c8
= <alexandressoares383@gmail.com>
2024-07-08 19:58:56 -0300

    feat: criando seu proprio hook

--- ignitegym/src/routes/index.tsx
+++ ignitegym/src/routes/index.tsx
@@ -2,15 +2,16 @@ import { useContext } from 'react';
 import { Box, useTheme } from 'native-base';
 import { DefaultTheme, NavigationContainer } from '@react-navigation/native';
 
-import { AuthContext } from '../contexts/AuthContext';
+import { useAuth } from '../hooks/useAuth';
 
 import { AuthRoutes } from './auth.routes';
 import { AppRoutes } from './app.routes';
 
 export function Routes() {
     const { colors } = useTheme();
-    const contextData = useContext(AuthContext);
-    console.log(contextData)
+    const { user } = useAuth();
+
+    console.log('USUARIO LOGADO', user)
 
     const theme = DefaultTheme;
     theme.colors.background = colors.gray[700];
