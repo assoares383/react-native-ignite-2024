c93ec14bc052238a38dd67530cb85d92f552e495
= <alexandressoares383@gmail.com>
2024-07-09 20:40:55 -0300

    feat: tratando exceções do handleSignIn

--- ignitegym/src/screens/SignIn.tsx
+++ ignitegym/src/screens/SignIn.tsx
@@ -1,10 +1,11 @@
 import { Controller, useForm } from "react-hook-form";
 import { useNavigation } from "@react-navigation/native";
-import { Center, Heading, Image, ScrollView, Text, VStack } from "native-base";
+import { Center, Heading, Image, ScrollView, Text, VStack, useToast } from "native-base";
 
 import { AuthNavigatorRoutesProps } from "../routes/auth.routes";
 
 import { useAuth } from "../hooks/useAuth";
+import { AppError } from "../utils/AppError";
 
 import LogoSvg from "@assets/logo.svg";
 import BackgroundImg from "@assets/background.png";
@@ -19,8 +20,8 @@ type FormData = {
 
 export function SignIn() {
   const { signIn } = useAuth();
-
   const navigation = useNavigation<AuthNavigatorRoutesProps>();
+  const toast = useToast();
 
   const {
     control,
@@ -33,7 +34,18 @@ export function SignIn() {
   }
 
   async function handleSignIn({ email, password }: FormData) {
-    await signIn(email, password)
+    try {
+      await signIn(email, password)
+    } catch (error) {
+      const isAppError= error instanceof AppError;
+      const title = isAppError ? error.message : 'Nao foi possivel entrar. Tente novamente mais tarde.'
+      
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500'
+      })
+    }
   }
 
   return (
