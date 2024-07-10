626470cc5252c6771fbefa3cafb68674bd5dbbc4
= <alexandressoares383@gmail.com>
2024-07-09 21:37:32 -0300

    feat: criando o metodo signOut

--- ignitegym/src/components/HomeHeader.tsx
+++ ignitegym/src/components/HomeHeader.tsx
@@ -1,41 +1,37 @@
 import { TouchableOpacity } from "react-native";
 import { Heading, HStack, Icon, VStack, Text } from "native-base";
-import { MaterialIcons } from '@expo/vector-icons';
+import { MaterialIcons } from "@expo/vector-icons";
 
 import { useAuth } from "../hooks/useAuth";
 
-import userPhotoDefault from '../assets/userPhotoDefault.png'
+import userPhotoDefault from "../assets/userPhotoDefault.png";
 
 import { UserPhoto } from "./UserPhoto";
 
 export function HomeHeader() {
-    const { user } = useAuth();
+  const { user, signOut } = useAuth();
 
-    return (
-        <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
-            <UserPhoto 
-                source={user.avatar ? { uri: user.avatar } : userPhotoDefault}
-                alt="Imagem do Usuario"
-                size={16}
-                mr={4}
-            />
-            <VStack flex={1}>
-                <Text color="gray.100" fontSize="md">
-                    Ola, 
-                </Text>
+  return (
+    <HStack bg="gray.600" pt={16} pb={5} px={8} alignItems="center">
+      <UserPhoto
+        source={user.avatar ? { uri: user.avatar } : userPhotoDefault}
+        alt="Imagem do Usuario"
+        size={16}
+        mr={4}
+      />
+      <VStack flex={1}>
+        <Text color="gray.100" fontSize="md">
+          Ola,
+        </Text>
 
-                <Heading color="gray.100" fontSize="md">
-                    { user.name }
-                </Heading>
-            </VStack>
-            
-            <TouchableOpacity>
-                <Icon 
-                    as={MaterialIcons}
-                    name="logout" color="gray.200"
-                    size={7}
-                />
-            </TouchableOpacity>
-        </HStack>
-    )
+        <Heading color="gray.100" fontSize="md">
+          {user.name}
+        </Heading>
+      </VStack>
+
+      <TouchableOpacity onPress={signOut}>
+        <Icon as={MaterialIcons} name="logout" color="gray.200" size={7} />
+      </TouchableOpacity>
+    </HStack>
+  );
 }
