a41d524e358eb48d42aa97853ca86621ae1d829c
= <alexandressoares383@gmail.com>
2024-07-10 20:10:09 -0300

    feat: buscando e listando grupos

--- ignitegym/src/screens/Home.tsx
+++ ignitegym/src/screens/Home.tsx
@@ -1,23 +1,47 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useNavigation } from '@react-navigation/native';
-import { FlatList, HStack, Heading, Text, VStack } from 'native-base';
+import { FlatList, HStack, Heading, Text, VStack, useToast } from 'native-base';
+
+import { api } from '../services/api';
 
 import { AppNavigatorRoutesProp } from '../routes/app.routes';
 
 import { ExerciseCard } from '../components/ExerciseCard';
 import { Group } from '../components/Group';
 import { HomeHeader } from '../components/HomeHeader';
+import { AppError } from '../utils/AppError';
 
 export function Home() {
-    const [groups, setGroups] = useState(['Costas', 'Biceps', 'Triceps', 'Ombro']);
+    const [groups, setGroups] = useState<string[]>([]);
     const [exercises, setExercises] = useState(['Puxada frontal', 'Remada curvada', 'Remada Unilateral', 'Levantamento terras']);
     const [groupSelected, setGroupSelected] = useState('Costas');
 
+    const toast = useToast();
     const navigation = useNavigation<AppNavigatorRoutesProp>();
     
     function handleOpenExerciseDetail() {
         navigation.navigate('exercise');
     }
+
+    async function fetchGroups() {
+        try {
+            const response = await api.get('/groups');
+            setGroups(response.data)
+        } catch (error) {
+            const isAppError = error instanceof AppError;
+            const title = isAppError ? error.message : 'Nao foi possivel carregar os grupos musculares';
+
+            toast.show({
+                title,
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        }
+    }
+
+    useEffect(() => {
+        fetchGroups()
+    }, [])
 
     return (
         <VStack flex={1}>
