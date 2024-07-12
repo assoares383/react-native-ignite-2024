b009b258bee74925495a6408b6a80f4f6fd168cc
= <alexandressoares383@gmail.com>
2024-07-12 19:08:09 -0300

    feat: carregando dados do usuário no profile

--- ignitegym/src/screens/Profile.tsx
+++ ignitegym/src/screens/Profile.tsx
@@ -7,10 +7,13 @@ import {
   Skeleton,
   Text,
   VStack,
-  useToast
+  useToast,
 } from "native-base";
+import { Controller, useForm } from "react-hook-form";
 import * as ImagePicker from "expo-image-picker";
 import * as FileSystem from "expo-file-system";
+
+import { useAuth } from "../hooks/useAuth";
 
 import { ScreenHeader } from "../components/ScreenHeader";
 import { UserPhoto } from "../components/UserPhoto";
@@ -19,6 +22,14 @@ import { Input } from "../components/Input";
 
 const PHOTO_SIZE = 33;
 
+type FormDataProps = {
+  name: string;
+  email: string;
+  password: string;
+  old_password: string;
+  confirm_password: string;
+}
+
 export function Profile() {
   const [photoIsLoading, setPhotoIsLoading] = useState(false);
   const [userPhoto, setUserPhoto] = useState(
@@ -26,6 +37,13 @@ export function Profile() {
   );
 
   const toast = useToast();
+  const { user } = useAuth();
+  const { control } = useForm({
+    defaultValues: {
+      name: user.name,
+      email: user.email
+    }
+  });
 
   async function handleUserPhotoSelect() {
     setPhotoIsLoading(true);
@@ -40,14 +58,16 @@ export function Profile() {
       if (photoSelected.canceled) return;
 
       if (photoSelected.assets[0].uri) {
-        const photoInfo = await FileSystem.getInfoAsync(photoSelected.assets[0].uri);
-        
-        if (photoInfo.size && (photoInfo.size / 1024 / 1024) > 5) {
+        const photoInfo = await FileSystem.getInfoAsync(
+          photoSelected.assets[0].uri
+        );
+
+        if (photoInfo.size && photoInfo.size / 1024 / 1024 > 5) {
           return toast.show({
             title: "Essa imagem e muito grande. Escolha uma de ate 5MB.",
-            placement: 'top',
-            bgColor: 'red.500'
-          })
+            placement: "top",
+            bgColor: "red.500",
+          });
         }
 
         setUserPhoto(photoSelected.assets[0].uri);
@@ -95,8 +115,32 @@ export function Profile() {
             </Text>
           </TouchableOpacity>
 
-          <Input bg="gray.600" placeholder="Nome" />
-          <Input bg="gray.600" placeholder="E-mail" isDisabled />
+          <Controller
+            control={control}
+            name="name"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                bg="gray.600"
+                placeholder="Nome"
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
+
+          <Controller
+            control={control}
+            name="email"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                bg="gray.600"
+                placeholder="E-mail"
+                isDisabled
+                onChangeText={onChange}
+                value={value}
+              />
+            )}
+          />
         </Center>
 
         <VStack px={10} mt={12} mb={9}>
