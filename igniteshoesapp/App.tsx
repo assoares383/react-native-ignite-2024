6483db9c166fbdbcad57f70d426c1a4d79edc15c
= <alexandressoares383@gmail.com>
2024-07-19 21:39:41 -0300

    chore: first commit

---
+++ igniteshoesapp/App.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'react-native';
+import { NativeBaseProvider } from 'native-base';
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
+
+import { OneSignal } from 'react-native-onesignal';
+
+import { Routes } from './src/routes';
+
+import { THEME } from './src/theme';
+import { Loading } from './src/components/Loading';
+
+import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize(`${process.env.ANDROID_KEY}`)
+
+export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
+
+  return (
+    <NativeBaseProvider theme={THEME}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <CartContextProvider>
+        {fontsLoaded ? <Routes /> : <Loading />}
+      </CartContextProvider>
+    </NativeBaseProvider>
+  );
+}
